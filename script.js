93d47a6403ef4fa7b5bc17b5faf0f87c3319ecb0
AvengersOF <kennedygamex@gmail.com>
2026-03-15 16:06:56 -0300

    Coloquei o código no lugar certo

--- script.js
+++ script.js
@@ -13,3 +13,48 @@ menu.style.left = "0px"
 }
 
 }
+
+function gerarCard (){
+
+let seed = Math.floor(Math.random()*100000)
+
+let avatar = document.getElementById("avatar")
+
+avatar.src = "https://api.dicebear.com/7.x/adventurer/svg?seed=" + seed
+
+
+let vida = Math.floor(Math.random()*100)+50
+let ataque = Math.floor(Math.random()*50)+10
+let defesa = Math.floor(Math.random()*50)+10
+let velocidade = Math.floor(Math.random()*40)+10
+
+
+document.getElementById("vida").innerText = vida
+document.getElementById("ataque").innerText = ataque
+document.getElementById("defesa").innerText = defesa
+document.getElementById("velocidade").innerText = velocidade
+
+
+let nomes = [
+"Drakon",
+"Zerath",
+"Kael",
+"Nyx",
+"Orion",
+"Ragnar",
+"Atlas",
+"Vex"
+]
+
+let nomeAleatorio = nomes[Math.floor(Math.random()*nomes.length)]
+
+document.getElementById("nome").innerText = nomeAleatorio
+
+
+let raridadeLista = ["Comum","Raro","Épico","Lendário"]
+
+let raridade = raridadeLista[Math.floor(Math.random()*raridadeLista.length)]
+
+document.getElementById("raridade").innerText = raridade
+
+}
